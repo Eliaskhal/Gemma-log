93efb2cfc66fb608f5fc25dee35e1a3ab280ba5f
Ilyas Khallouki <khalloukielias@gmail.com>
2024-06-26 10:06:52 +0100

    Updated the downloader

--- downloader.py
+++ downloader.py
@@ -1,5 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-model_name = "bertin-project/gemma-2b-it"
+model_name = "google/gemma-2b-it"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.bfloat16
+)
